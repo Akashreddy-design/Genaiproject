e0dc0a7ef7884ef6ad1b5e2c116654b0bdcc5679
Akash Reddy Kontham <akash.kontham@gmail.com>
2025-07-12 10:39:37 +0530

    Initial commit - rewritten

---
+++ chatbot/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True,blank=True, on_delete = models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    product_id = models.CharField(max_length=1000, null=True)
+    name= models.CharField(max_length=100, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    price = models.CharField(max_length=200,null=False, blank=False)
+    description = models.TextField(max_length=500, null=True)
+    rating = models.CharField(max_length=50, null=True)
+    rating_count = models.CharField(max_length=200,null=True)
+    image_link = models.CharField(max_length=500, null=True)
+    product_link = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add = True)
+    def __str__(self):
+        return self.product.name
+
+
+
---
+++ chatbot/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.loginPage, name="login"),
+    path('register/', views.registerPage, name = "register"),
+    path('home/', views.home, name="home"),
+    path('chat/', views.chat, name="chat"),
+    path('profile/', views.profile, name='profile'),
+    path('logout', views.logoutPage, name="logout"),
+    # path('recommendation/', views.product_recommendation_view, name='product_recommendation'),
+]
